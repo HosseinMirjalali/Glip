98617da594378022f87d09cf0f2cd0e078dadddc
hosermes <mirjalali.hosein@gmail.com>
2021-06-24 13:26:36 +0430

    updated follow page, navbar, and url namespaces

--- glip/clips/urls.py
+++ glip/clips/urls.py
@@ -4,6 +4,6 @@ from .views import follows_view, my_view
 
 app_name = "clips"
 urlpatterns = [
-    path("follow", view=follows_view, name="clips"),
-    path("followed", view=my_view, name="followed"),
+    path("follow", view=follows_view, name="followed"),
+    path("api/followed", view=my_view, name=""),
 ]
