e33d3f6b3fec56421129e581b95ef12677f4a978
hosermes <mirjalali.hosein@gmail.com>
2021-07-27 12:38:59 +0430

    yourclips view now uses async game clips and is faster

--- glip/clips/views.py
+++ glip/clips/views.py
@@ -185,7 +185,7 @@ def game_clip_page(request):
 def your_clip_page(request):
     template_name = "pages/clip.html"
     user_token = get_token(request)
-    user_game_follows_clips = get_user_game_follows_clips(request, user_token)
+    user_game_follows_clips = get_followed_games_clips_async(request, user_token)
     user_channel_follows = get_user_follows2(request, user_token)
     clips = get_user_games_channels_clips(user_game_follows_clips, user_channel_follows)
     return render(request, template_name, {"clips": clips})
