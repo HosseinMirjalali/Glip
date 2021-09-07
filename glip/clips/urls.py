0868d91788734c7165e0d41d67e9893404f85828
hosermes <mirjalali.hosein@gmail.com>
2021-09-07 12:09:47 +0430

    added feed url and new locked task for testing

--- glip/clips/urls.py
+++ glip/clips/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import clips_view, futures_followed_clips, your_clip_page, new_your_clips_local
+from .views import clips_view, futures_followed_clips, your_clip_page, new_your_clips_local, feed_view
 
 app_name = "clips"
 urlpatterns = [
     # re_path(r'^follow/(?P<game_id>[\d]+)/$', follow_game, name="follow_game"),
     path("clips", view=clips_view, name="clips"),
     path("future", view=futures_followed_clips, name="future"),
+    path("feed", view=feed_view, name="feed"),
     path("yourclips/", view=your_clip_page, name="your_clips"),
     path("yourclipsnew/", view=new_your_clips_local, name="new_your_clips"),
 ]
