6467ce585dd67100e5de60970dc376e562a0d002
hosermes <mirjalali.hosein@gmail.com>
2021-09-26 11:17:00 +0330

    added local games clip page for no auth games page

--- glip/games/urls.py
+++ glip/games/urls.py
@@ -4,6 +4,7 @@ from .views import (
     follow_game,
     game_clip_page,
     games_view,
+    local_game_clip_view,
     noauthgameslist,
     unfollow_game,
 )
@@ -15,4 +16,5 @@ urlpatterns = [
     path("follow_game", view=follow_game, name="follow_game"),
     path("unfollow_game", view=unfollow_game, name="unfollow_game"),
     path("gameclip/", view=game_clip_page, name="gameclip"),
+    path("clips/", view=local_game_clip_view, name="localgameclip"),
 ]
