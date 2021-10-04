da379d7310640057b65df654a6c12c4f9784ab33
hosermes <mirjalali.hosein@gmail.com>
2021-10-04 17:41:56 +0330

    homepage now shows see comments which leads to clip detail page

--- glip/comments/forms.py
+++ glip/comments/forms.py
@@ -7,4 +7,14 @@ class NewCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("content",)
-        widgets = {"content": forms.Textarea(attrs={"class": "form-control"})}
+        widgets = {
+            "content": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Your comment...",
+                    "rows": 4,
+                    "cols": 10,
+                }
+            )
+        }
+        labels = {"content": ""}
