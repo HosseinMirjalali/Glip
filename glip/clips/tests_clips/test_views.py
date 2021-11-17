ce65b35e0740020747dc127239de0c858779dce7
hossein <mirjalali.hosein@gmail.com>
2021-11-17 23:22:20 +0330

    added likes tests, and removed deprecated assertEquals

--- glip/clips/tests_clips/test_views.py
+++ glip/clips/tests_clips/test_views.py
@@ -208,7 +208,7 @@ class TestClipDetailView(TestCase):
         url = reverse("clips:clip_detail", args=[self.clip.clip_twitch_id])
         self.c.login(username="test", password="top_secret")
         response = self.c.get(url)
-        assert response.context["fav"] is True
+        assert response.context["fav"]
 
     @pytest.mark.client
     def test_comment_is_shown(self):
@@ -341,7 +341,7 @@ class TestClipsForYou(TestCase):
 
 class TestFeedView(TestCase):
     """
-    Tests regarding ClipsForYou view ( feed_view )
+    Tests regarding feed view ( feed_view )
     """
 
     def setUp(self):
@@ -489,9 +489,137 @@ class TestFeedView(TestCase):
         url = reverse("clips:feed")
         self.c.login(username="test", password="top_secret")
         response = self.c.get(url)
-        self.assertEquals(response.status_code, 200)
+        assert response.status_code == 200
         assert response.context["clips"][0].likes_count == 1
         assert response.context["clips"][1].likes_count == 0
-        assert response.context["clips"][0].fav is True
+        assert response.context["clips"][0].fav
         assert response.context["clips"][1].fav is False
         assert response.context["clips"][0] == self.clip
+
+
+class TestLikeClip(TestCase):
+    """
+    Tests regarding liking a clip (like_clip)
+    """
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.c = Client()
+        self.user = User.objects.create_user(username="test", email="test@test.com")
+        self.user.set_password("top_secret")
+        self.user.save()
+        self.game = Game.objects.create(
+            game_id="461067",
+            name="Tekken 7",
+            box_art_url="https://static-cdn.jtvnw.net/ttv-boxart/Tekken%207-{width}x{height}.jpg",
+        )
+        self.clip = Clip.objects.create(
+            clip_twitch_id="ZealousVictoriousSamosaPlanking",
+            url="https://clips.twitch.tv/ZealousVictoriousSamosaPlanking",
+            embed_url="https://clips.twitch.tv/embed?clip=ZealousVictoriousSamosaPlanking",
+            broadcaster_id="4254625231",
+            broadcaster_name="Kenoq_1",
+            creator_id="2302867421",
+            creator_name="madara_irgen1",
+            video_id="11708822291",
+            twitch_game_id="4610671",
+            game=self.game,
+            language="nl",
+            title="Chair get destroyed by angry fist that once destroyed closet1",
+            twitch_view_count="9",
+            glip_view_count="",
+            created_at=timezone.now() - timezone.timedelta(hours=2),
+            thumbnail_url="https://clips-media-assets2.twitch.tv/AT-cm%7CHSCQyY59sYdqFvjRu4CUHQ-preview-480x272.jpg",
+            duration="17.3",
+            like_count=0,
+            disabled=False,
+        )
+        self.clip_other = Clip.objects.create(
+            clip_twitch_id="ZealousVictoriousSamosaPlanking2",
+            url="https://clips.twitch.tv/ZealousVictoriousSamosaPlanking2",
+            embed_url="https://clips.twitch.tv/embed?clip=ZealousVictoriousSamosaPlanking2",
+            broadcaster_id="42546252312",
+            broadcaster_name="Kenoq_12",
+            creator_id="23028674212",
+            creator_name="madara_irgen12",
+            video_id="117088222912",
+            twitch_game_id="46106712",
+            game=self.game,
+            language="nl",
+            title="Chair get destroyed by angry fist that once destroyed closet12",
+            twitch_view_count="9",
+            glip_view_count="",
+            created_at=timezone.now() - timezone.timedelta(hours=2),
+            thumbnail_url="https://clips-media-assets2.twitch.tv/AT-cm%7CHSCQyY59sYdqFvjRu4CUHQ-preview-480x272.jpg",
+            duration="17.3",
+            like_count=0,
+            disabled=False,
+        )
+        self.clip_old = Clip.objects.create(
+            clip_twitch_id="jn2xh2oW",
+            url="https://clips.twitch.tv/jn2xh2oW",
+            embed_url="https://clips.twitch.tv/embed?clip=jn2xh2oW",
+            broadcaster_id="425462522",
+            broadcaster_name="Kenoq__",
+            creator_id="2302867422",
+            creator_name="madara_irgenn",
+            video_id="11708822299",
+            twitch_game_id="4610677",
+            game=self.game,
+            language="nl",
+            title="Chair get destroyed by angry fist that once destroyed closet ",
+            twitch_view_count="8",
+            glip_view_count="",
+            created_at=timezone.now() - timezone.timedelta(weeks=2),
+            thumbnail_url="https://clips-media-assets2.twitch.tv/AT-cm%7CHSCQyY59sYdqFvjRu4CUHQ-preview-480x272.jpg",
+            duration="17.3",
+            like_count=0,
+            disabled=False,
+        )
+        self.social_account = SocialAccount.objects.create(
+            user=self.user, provider="twitch"
+        )
+        self.social_app = SocialApp.objects.create(
+            provider="twitch", name="twitch", client_id="client_id1234", secret="key264"
+        )
+        self.social_token = SocialToken.objects.create(
+            app=self.social_app, account=self.social_account, token="2323token"
+        )
+        self.top_clip = TopClip.objects.create(clip=self.clip)
+        self.top_clip_other = TopClip.objects.create(clip=self.clip_other)
+        self.top_clip_old = TopClip.objects.create(clip=self.clip_old)
+        # self.like = self.clip.likes.add(self.user)
+        self.comment = Comment.objects.create(
+            user=self.user,
+            clip=self.clip,
+            reply=None,
+            content="Test123",
+            timestamp=timezone.now(),
+        )
+
+    @pytest.mark.client
+    def test_authed_user(self):
+        """
+        Test that an authenticated user can like a clip
+        :return:
+        """
+        url = reverse("clips:like-clip")
+        self.c.login(username="test", password="top_secret")
+        response = self.c.post(
+            url, data={"clip_id": self.clip.clip_twitch_id, "action": "post"}
+        )
+        assert response.status_code == 200
+        assert self.clip.likes.filter(id=self.user.id).exists()
+
+    @pytest.mark.client
+    def test_no_auth_user(self):
+        """
+        Test that a non-authorized user can NOT like a clip
+        :return:
+        """
+        url = reverse("clips:like-clip")
+        response = self.c.post(
+            url, data={"clip_id": self.clip.clip_twitch_id, "action": "post"}
+        )
+        assert response.status_code == 302
+        assert self.clip.likes.count() == 0
